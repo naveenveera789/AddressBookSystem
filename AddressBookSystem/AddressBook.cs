e6985bcc99076fed38e4524bb155e851c2f0c838
naveenveera789 <naveenveera789@gmail.com>
2021-11-17 16:18:55 +0530

    [naveenveera789] add : The Add contact program is done

---
+++ AddressBookSystem/AddressBook.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookSystem
+{
+    public class AddressBook
+    {
+        List<Contact> addressList = new List<Contact>();
+        public void AddContact(Contact contact)
+        {
+            addressList.Add(contact);
+        }
+        public void Display()
+        {
+            foreach (var contact in addressList)
+            {
+                Console.WriteLine(contact.FirstName + "\t" + contact.LastName + "\t" + contact.City + "\t" + contact.PhoneNumber);
+            }
+        }
+    }
+}
--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -7,6 +7,8 @@ namespace AddressBookSystem
         static void Main(string[] args)
         {
             bool flag = true;
+            Contact contact = new Contact();
+            AddressBook addressBook = new AddressBook();
             while (flag)
             {
                 Console.WriteLine("Welcome to the Address Book Program");
@@ -16,7 +18,7 @@ namespace AddressBookSystem
                 {
                     case 1:
                         Console.WriteLine("Enter the Contact details of FirstName, LastName, Address, City, State, Zip, Ph.no, Email : ");
-                        Contact contact = new Contact()
+                        contact = new Contact()
                         {
                             FirstName = Console.ReadLine(),
                             LastName = Console.ReadLine(),
@@ -29,6 +31,22 @@ namespace AddressBookSystem
                         };
                         break;
                     case 2:
+                        Console.WriteLine("Enter the Contact Information to be Add in form of FirstName, LastName, Addr, City, State, Zip, Ph.No, Email.");
+                        contact = new Contact()
+                        {
+                            FirstName = Console.ReadLine(),
+                            LastName = Console.ReadLine(),
+                            Address = Console.ReadLine(),
+                            City = Console.ReadLine(),
+                            State = Console.ReadLine(),
+                            Zip = Console.ReadLine(),
+                            PhoneNumber = Console.ReadLine(),
+                            Email = Console.ReadLine(),
+                        };
+                        addressBook.AddContact(contact);
+                        addressBook.Display();
+                        break;
+                    case 4:
                         flag = false;
                         break;
                 }
